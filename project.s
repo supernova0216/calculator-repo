76ba63595b87f7bef9fc32e22d1c70aba284638b
supernova0216 <150007006+supernova0216@users.noreply.github.com>
2024-12-01 02:30:53 -0800

    Updated project.s
    adjusted square root, organized logical operation, and added exponentiation

--- project.s
+++ project.s
@@ -17,14 +17,16 @@
 	binary_store:		.space 32
 	decimal_store:		.fill 1, 8, 0
 	temp_store:			.fill 1, 4, 0
-	num:				.double 0.0	//fill 1, 8, 0
+	num:				.double 0.0	
+	prev_result:		.double 0.0
 	
 
 	
 .text
 
 main: 
-			
+		//stp lr, x5, [sp, #-16]
+		//ldp lr, x5, [sp], #16		
 calculator_off:
 	printStr "Please enter 1 to start calculator"
 	printStr "Enter -1 to turn off"
@@ -69,11 +71,14 @@ calculator_on:
 	cmp w0, #0
 	beq exit_program
 	
-	b ABNORMAL_OPERATION
+	printStr "Invalid input."
+	b calculator_on
+
 	
 calc_expression:
 	printStr "In calc expression"
 	b calculator_on
+
 	
 basic_arithmetic:
 				//start enter 2 numbers
@@ -86,8 +91,6 @@ first_num:
 	str d0, [x1]
 	
 	fmov d3, d0
-	
-	//bl print
 	
 	sub sp, sp, #16
 	str d0, [sp]
@@ -164,28 +167,36 @@ multiplication:
 	b next
 
 division:
+	fcmp d4, 0
+	beq undefined
+	
 	fdiv d0, d3, d4	
 	stp d3, d4, [sp, #-16]! 	
 	bl print
 
 	b next
 	
+undefined:
+	stp d3, d4, [sp, #-16]! 
+	printStr "Undefined. Divisor must be not equal to 0"
+	b next
+	
 next:	
-	//stp d3, d4, [sp, #-16]!
+	
 	ldr x0, =temp_store
-	printStr "\nEnter 2 to continue using same numbers for other operations"
-	printStr "Enter 1 to input new numbers/ back to operations menu"
-	printStr "Any number to go back Main Menu"
+	printStr "\nEnter 1 to continue using same numbers for other operations"
+	printStr "Enter 2 to input new numbers/ back to operations menu"
+	printStr "Any number to go back to Main Menu"
 	bl get_input
 	
 	ldr x2, =temp_store
 	str x0, [x2]
 	ldp d3, d4, [sp], #16
 	
-	cmp x0, #2
+	cmp x0, #1
 	beq operations
 	
-	cmp x0, #1
+	cmp x0, #2
 	beq basic_arithmetic
 	
 	b calculator_on
@@ -196,15 +207,18 @@ sub_menu:
 	
 	printStr "Sub-Menu"
 	printStr "Enter 1 to do Modulo ( % )"
-	printStr "Enter 2 to Find Square Roots"
-	printStr "Enter 3 to Convert Decimal to binary"
+	printStr "Enter 2 to find Square Roots"
+	printStr "Enter 3 to convert Decimal to binary"
 	printStr "Enter 4 to convert Binary to Decimal"
 	printStr "Enter 5 to convert Decimal to Hex"
-	printStr "Enter 6 to Convert Hex to Decimal"
+	printStr "Enter 6 to convert Hex to Decimal"
 	printStr "Enter 7 for Unit Conversion"
-	printStr "Enter 8 to return back to Main Menu"
+	printStr "Enter 8 to find value of Exponent numbers"
+	printStr "Enter 9 to find Factorial"
+	printStr "Any number to return back to Main Menu"
 	
 	bl get_input
+	
 	cmp w0, #1
 	beq calc_mod
 	
@@ -227,9 +241,14 @@ sub_menu:
 	beq convert_units
 	
 	cmp w0, #8
-	beq calculator_on
+	beq exponent
 	
-	b ABNORMAL_OPERATION
+	cmp w0, #9
+	beq factorial
+	
+	b calculator_on
+	
+	//b ABNORMAL_OPERATION
 	
 	
 calc_mod: // mod is remainder (a mod b = a - (a/b)*b
@@ -273,15 +292,36 @@ calc_mod: // mod is remainder (a mod b = a - (a/b)*b
 	bl print_modulo
 	printStr ""
 	b sub_menu
+
+	
 calc_sqrt:
-	printStr "in sqrt"
+	ldr x0, =num
+	printStr "Enter a number for a square root: "
+	bl get_num
+	
+	ldr x1, =num
+	str d0, [x1]
+	fcmp d0, 0
+	blt error
+	
+	fmov d5, d0
+	fsqrt d0, d5
+	bl print
+	sub sp, sp, #16
+	str d0, [sp]
 	b sub_menu
+
+error:
+	printStr "Error: Input must be positive."
+	b calc_sqrt
 	
 convert_deci_binary:
 	printStr "Enter a Decimal Number to Convert to Binary"
 	
 	bl print_binary
 	b sub_menu
+
+	
 convert_binary_deci:
 	printStr "Enter Binary Number to Convert to Decimal"
 	ldr x0, =decimal_store
@@ -295,6 +335,7 @@ convert_binary_deci:
 	mov w2, #2 // used to multiply by 2
 	mov w6, w0 // w6 = w0, temp = input // a
 	mov w7, #10 // b
+	
 convert_loop: // a - (a / b) * b
 	sdiv w8, w6, w7 // (a / b)
 	mul w8, w8, w7 // (a / b) * b
@@ -319,21 +360,112 @@ convert_loop: // a - (a / b) * b
 	bl print_decimal
 	
 	b sub_menu
+	
+	
 convert_deci_hex:
 	printStr "in convert deci to hex"
 	b sub_menu
+	
 	
 convert_hex_deci:
 	printStr "in hex to deci"
 	b sub_menu
 	
+	
 convert_units:
 	printStr "in convert units"
 	b sub_menu
+	
+
+exponent:
+	ldr x0, =num
+	printStr "Enter base: "			//data type double
+	bl get_num
+	
+	ldr x1, =num
+	str d0, [x1]
+	fmov d5, d0
+	sub sp, sp, #16
+	str d0, [sp]
+
+
+	ldr x0, =num
+	printStr "Enter exponent: "
+	bl get_num
+	
+	ldr x2, =num
+	str d0, [x2]
+	fmov d6, d0
+	
+	sub sp, sp, #16
+	str d0, [sp]
+	
+	ldr d6, [sp]
+	add sp, sp, #16
+	ldr d5, [sp]
+	
+	fmov d7, 1				//d7 stores exponential result, assign it = 1
+	fmov d8, 1
+	
+	fcmp d6, 0
+	beq special1	
+	blt negative	
+	
+	fcmp d6, d8
+	beq special2	
+	
+	fmov d9, 1			//d9 = 1 as d6 is positive
+loop:	
+	fmul d7, d7, d5 
+	fsub d6, d6, d8
+	fcmp d6, 0
+	beq expo_result1
+	
+	b loop 		
+	
+special1:			
+	fmov d0, d7
+	bl print
+	
+	b sub_menu
+	
+special2:
+	fmov d7, d5			
+	fmov d0, d7
+	bl print
+	
+	b sub_menu
+
+negative:
+	//printStr "Error: Calculator is now working with positive exponent only"
+	//b exponent
+	fmov d9, -1			//d9 = -1 as d6 is negative
+	fabs d6, d6
+	b loop
+
+expo_result1:
+	fcmp d9, d8
+	bne expo_result2
+	
+	fmov d0, d7
+	bl print
+	
+	b sub_menu
+
+expo_result2:
+	fdiv d0, d8, d7
+	bl print
+	
+	b sub_menu
+	
+factorial:
+
+	
 ABNORMAL_OPERATION:
 	printStr "CALCULATOR FAULT"
 	//printStr "TURNING OFF NOW"
 	b calculator_on
+ 
       
 exit_program:
 	printStr "Turning calculator off.."
